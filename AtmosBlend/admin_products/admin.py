2720ed2ff159dde7c54d8c5e9d5b00fba1110d77
Gowtham K M <116723241+Gowthamk007m@users.noreply.github.com>
2024-03-17 23:04:22 +0530

    2nd

--- AtmosBlend/admin_products/admin.py
+++ AtmosBlend/admin_products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Brand)
+admin.site.register(models.Categories)
+admin.site.register(models.Product)
+admin.site.register(models.Product_Image)
+
+
+
+from admin_variant.models import *
+
+admin.site.register(Product_Variant)
+admin.site.register(Colors)
